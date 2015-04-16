1b6aa40f645c82ea5255e876d185f2f644b66a4b
afadhilen <fadhil.kreydle@gmail.com>
2015-04-16 17:07:54 +0800

    title index change

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Book Record Data</title>
+        <title>LIMAS</title>
 
         <link rel="stylesheet" type="text/css" href="style.css">        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
